a25305669546dc770f18b895a8c8072267d79b1c
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-07-02 17:53:38 +0900

    1st commit

---
+++ src/main/java/com/mijinco0612/springauth0/SpringAuth0Application.java
@@ -0,0 +1,12 @@
+package com.mijinco0612.springauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuth0Application.class, args);
+	}
+}
