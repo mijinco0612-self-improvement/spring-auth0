91cf51290bb389a4417d580743de8beb0f2e3a87
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-07-04 19:52:31 +0900

    verify jwt

--- src/main/java/com/mijinco0612/springauth0/security/WebSecurityConfig.java
+++ src/main/java/com/mijinco0612/springauth0/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.mijinco0612.springauth0.security;
 
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,11 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${auth0.issuer}")
+    private String issuer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/v1/public").permitAll()
                 .antMatchers("/**").authenticated();
+
+        JwtWebSecurityConfigurer
+                .forRS256(audience, issuer)
+                .configure(http);
     }
 }
